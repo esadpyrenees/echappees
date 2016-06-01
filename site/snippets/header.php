be27831cd1bf28c958f0612f5f0ee1595263366f
Julien Bidoret <julienbidoret@gmail.com>
2016-06-01 12:13:37 +0200

    Massive attack

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -17,14 +17,20 @@
 	<meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
   
     <!-- appel des fonts -->
-    <link href='https://fonts.googleapis.com/css?family=Neuton:400,200,300,400italic,700,800' rel='stylesheet' type='text/css'>
+    <!-- <link href='https://fonts.googleapis.com/css?family=Neuton:400,200,300,400italic,700,800' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Magra:400,700' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Raleway:400,900italic,900,800,800italic,700italic,700,600italic,600,500italic,400italic,500,300italic,300,200italic,200,100italic,100' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Kreon:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Kreon:400,700,300' rel='stylesheet' type='text/css'> -->
   
 	<!-- script pour type kit -->
-    <script src="https://use.typekit.net/pkn1ilg.js"></script>
+    <!-- kit original (luminance) -->
+    <!-- <script src="https://use.typekit.net/pkn1ilg.js"></script> -->
+    <!-- nouveau kit de test -->
+    <script src="https://use.typekit.net/rhc4ddt.js"></script>
 	<script>try{Typekit.load({ async: true });}catch(e){}</script>
+    
+    
+    
     
 	<!-- appel d'une bibliotheque pour la fonction plier deplier de la page Auteur et note -->
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
@@ -34,6 +40,13 @@
     <!-- le reset permet de retirer les paramètrages par default -->
 	<?php echo css('assets/css/reset.css') ?>
 	<!-- appel du css général -->
+    
+    <?php 
+        $ff  = param('ff'); 
+        echo css('assets/css/typo-'.$ff.'.css');
+    ?>
+    
+
 	<?php echo css('assets/css/styleGeneral.css') ?>
 	<!-- le responsive permet de gérer l'ensemble des éléments qui subissent une transformation 
 	lors d'une modifictaion de tailles d'ecran -->
@@ -45,7 +58,6 @@
 </head>
 <!-- on ouvre le body, il est fermé dans le footer -->
 <body >
-    
    
 <div id="menu-overlay"></div>
 	
